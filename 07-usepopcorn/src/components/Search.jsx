04f5bcc700ca1be56e38bdbd85eb487ed1f76f22
Md Imran <samimdimran786@gmail.com>
2026-02-10 20:56:15 +0530

    useRef hook

--- 07-usepopcorn/src/components/Search.jsx
+++ 07-usepopcorn/src/components/Search.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useRef } from "react";
+
 const Search = ({ query, setQuery }) => {
+  const searchRef = useRef(null);
+
+  useEffect(() => {
+    const eventListenerForEnter = (e) => {
+      if (document.activeElement === searchRef.current) return;
+
+      if (e.key == "Enter") {
+        setQuery("");
+        searchRef.current.focus();
+      }
+    };
+    document.addEventListener("keydown", eventListenerForEnter);
+    searchRef.current.focus();
+
+    return () => {
+      document.removeEventListener("keydown", eventListenerForEnter);
+    };
+  }, []);
   return (
     <div>
       <input
         type="text"
         className="search"
         placeholder="Search movies..."
+        ref={searchRef}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
