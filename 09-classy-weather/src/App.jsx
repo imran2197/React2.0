77175e0e61b7a401723adfd6979f55c7c0021a7c
Md Imran <samimdimran786@gmail.com>
2026-02-11 13:19:16 +0530

    class fields

--- 09-classy-weather/src/App.jsx
+++ 09-classy-weather/src/App.jsx
@@ -33,18 +33,14 @@ function formatDay(dateStr) {
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: "London",
-      isLoading: false,
-      displayLocation: "",
-      weather: {},
-    };
-    this.fetchWeather = this.fetchWeather.bind(this);
-  }
+  state = {
+    location: "London",
+    isLoading: false,
+    displayLocation: "",
+    weather: {},
+  };
 
-  async fetchWeather() {
+  fetchWeather = async () => {
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -73,7 +69,7 @@ class App extends Component {
     } finally {
       this.setState({ isLoading: false });
     }
-  }
+  };
 
   render() {
     return (
