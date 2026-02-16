589a1b9e3838421d01826f9c990942b86b9619c1
Md Imran <samimdimran786@gmail.com>
2026-02-16 18:15:36 +0530

    integrated leaflet map

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,23 +1,33 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+
 const Map = () => {
+  const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
-  const navigate = useNavigate();
-
-  console.log(lat, lng);
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
-        Change POS
-      </button>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>You are here 📍</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
