290c988c2f2fbfd6f8f9d2bc007e895b86b36168
Md Imran <imran251099@gmail.com>
2026-06-16 15:35:34 +0530

    old redux template code

--- 15-redux-intro/src/App.js
+++ 15-redux-intro/src/App.js
@@ -1,17 +1,24 @@
-import CreateCustomer from "./CreateCustomer";
-import Customer from "./Customer";
-import AccountOperations from "./AccountOperations";
-import BalanceDisplay from "./BalanceDisplay";
+import CreateCustomer from "./features/customers/CreateCustomer";
+import Customer from "./features/customers/Customer";
+import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import AccountOperations from "./features/accounts/AccountOperations";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((store) => store.customer.fullName);
   return (
-    <div>
+    <>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
-    </div>
+      {!fullName ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
+    </>
   );
 }
 
