2953a06d0306b94a782d5032ee585a8c1b9709bf
Md Imran <imran251099@gmail.com>
2026-06-19 09:53:28 +0530

    Tailwind Css Colors

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 const Header = () => {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza Co.</Link>
 
-      <p>Md Imran</p>
-
       <SearchOrder />
+
+      <p>Md Imran</p>
     </header>
   );
 };
